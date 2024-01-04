e374eb64241a65da95a2cea614c0c6ca29540a7b
Keach <69722242+keach9180@users.noreply.github.com>
2024-01-04 07:48:01 +0000

    Squashed commit of the following:
    commit f731d502b93b72896a2ff0d6e7f826945b8207ac
    Author: Keach <69722242+keach91@users.noreply.github.com>
    Date:   Wed Jan 3 11:04:55 2024 +0000
    
        Enable vscode plugin
    
    commit 97965ea3823d63b57cbfb60c2ceb19a627af2383
    Author: Keach <69722242+keach91@users.noreply.github.com>
    Date:   Tue Jan 2 12:51:31 2024 +0000
    
        Add zsh aliases for chezmoi
    
    commit dcef300290de63585ccfa753d4bb3bcc1e58258e
    Author: Keach <69722242+keach91@users.noreply.github.com>
    Date:   Sun Dec 31 08:15:16 2023 +0000
    
        Create chezmoi plugin for oh-my-zsh
    
    commit 9525865a1268a45ab3a1e8f9d961ab05a90bdfa8
    Author: Keach <69722242+keach91@users.noreply.github.com>
    Date:   Sun Dec 31 07:37:27 2023 +0000
    
        Mod ZSH_CUSTOM dir to ~/.config/oh-my-zsh/custom
    
    commit a402c42e340426449f912375ead0d972e8f42a1e
    Author: Keach <69722242+keach91@users.noreply.github.com>
    Date:   Sat Dec 30 07:41:15 2023 +0000
    
        Create dot_zsh.d dir as .oh-my-zsh/custom
    
    commit cc3430b89d8719a28234ccfb5ea036fb63f0dcfa
    Author: Keach <69722242+keach91@users.noreply.github.com>
    Date:   Sat Dec 30 06:46:32 2023 +0000
    
        Add scripts dir into .chezmoiignore
    
    commit db98c033befaf3499440ede8065b27ee93c3d0c6
    Author: Keach <69722242+keach91@users.noreply.github.com>
    Date:   Sun Nov 26 09:35:09 2023 +0000
    
        Create install-fonts.sh
    
    commit c80d3d1893bae73dff28a58d592fcc9a282c4ab2
    Author: Keach <69722242+keach91@users.noreply.github.com>
    Date:   Sun Nov 26 08:23:42 2023 +0000
    
        Add editor setting for VSCode

---
+++ dot_config/oh-my-zsh/custom/plugins/chezmoi/chezmoi.plugin.zsh
@@ -0,0 +1,11 @@
+source "${0:A:h}/chezmoi.completion.zsh"
+
+alias cz="chezmoi"
+alias cza="chezmoi add"
+alias czap="chezmoi apply"
+alias czcd="chezmoi cd"
+alias czd="chezmoi diff"
+alias cze="chezmoi edit"
+alias czg="chezmoi git"
+alias czrm="chezmoi remove"
+alias czst="chezmoi status"
